44451e93d39af6f64aab9992f376a64197d1d7aa
aguigui <pascal.aguilar@gmail.com>
2019-11-07 16:15:32 +0100

    voila

--- App.js
+++ App.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import Navigation from './Navigation/Navigation'
 
- 
+ // commentaire super style
 export default class App extends React.Component {
   render() {
     return (
